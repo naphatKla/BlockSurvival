35f20d4eab7dec3c823d428f0fba67797b5f4e7d
kla <naphat.watt@bumail.net>
2023-04-21 13:43:13 +0700

    Enemy follow target system added
    - add method FollowTargetHandle in Enemy.cs

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,17 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Enemy Stats")]
+    [SerializeField] private float maxHp;
+    [SerializeField] private float attackDamage;
+    
+    [Header("Movement")]
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float minSpeed;
+    
+    // The condition of distance for change enemy speed depend on target distance
+    [SerializeField] private float distanceThreshold;
+    
+    [Header("Other")]
+    // Must change when player is done.
+    [SerializeField] private GameObject player;
+    private float _currentSpeed;
+    private float _currentHp;
     void Start()
     {
-        
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        FollowTargetHandle(player);
+    }
+
+    
+    float _smoothDampVelocity;
+    private void FollowTargetHandle(GameObject target)
+    {
+        Vector2 enemyPosition = transform.position;
+        Vector2 targetPosition = target.transform.position;
+        float distanceToTarget = Vector2.Distance(enemyPosition, targetPosition);
         
+        if (distanceToTarget <= 0) return;
+        
+        enemyPosition =
+            Vector2.MoveTowards(enemyPosition, targetPosition, _currentSpeed * Time.deltaTime);
+        transform.position = enemyPosition;
+        
+        _currentSpeed = distanceToTarget > distanceThreshold
+            ? Mathf.SmoothDamp(_currentSpeed, maxSpeed, ref _smoothDampVelocity, 0.3f)
+            : Mathf.SmoothDamp(_currentSpeed, minSpeed, ref _smoothDampVelocity, 0.3f);
     }
 }
