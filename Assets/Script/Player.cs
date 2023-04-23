bd2719c8cef9483b0741fea096878e6f4c5c129c
kla <naphat.watt@bumail.net>
2023-04-23 12:47:51 +0700

    Player Movement Fixed & Rework
    - Clean some code
    - Add player status
    - Add player tired when stamina = 0
    - Add Hp bar

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -12,108 +12,176 @@ public class Player : MonoBehaviour
     [SerializeField] private Rigidbody2D playerRigidbody2D;
     [SerializeField] private TrailRenderer trailEffect;
 
-    [Header("PlayerStat")] 
+    [Header("Player Stats")] 
+    [SerializeField] private float maxHealth;
     [SerializeField] private float maxStamina;
-    [SerializeField] private float stamina;
     [SerializeField] private float staminaRegen;
-    [SerializeField] private float dashStaminaDrain;
-    [SerializeField] private float sprintStaminaDrain;
-
+    private float _health;
+    private float _stamina;
+    
     [Header("UI Bar")] 
+    [SerializeField] private Scrollbar healthBar;
+    [SerializeField] private TextMeshProUGUI healthText;
     [SerializeField] private Scrollbar staminaBar;
     [SerializeField] private TextMeshProUGUI staminaText;
 
     [Header("Camera")] 
     [SerializeField] private Camera playerCamera;
     [SerializeField] private float cameraSmoothDamp;
-    private Vector2 _velocity = Vector2.zero;
+    private Vector3 _velocity = Vector3.zero;
 
-    [Header("Movement")] 
+    [Header("Player Movement")] 
+    [SerializeField] private KeyCode sprintKey;
+    [SerializeField] private KeyCode dashKey;
     [SerializeField] private float walkSpeed;
     [SerializeField] private float sprintSpeed;
+    [SerializeField] private float dashStaminaDrain;
+    [SerializeField] private float sprintStaminaDrain;
     [SerializeField] private float dashSpeed;
     [SerializeField] private float dashDuration;
+    [SerializeField] private float staminaRecoveryCooldown;
     private float _currentSpeed;
-    private float _dashTime;
-    private bool _isDash;
-    private bool _isSprint;
+    public PlayerStatus playerStatus;
+    
+    public enum PlayerStatus
+    {
+        Clear,
+        Idle,
+        Walk,
+        Sprint,
+        Dash,
+        Stun,
+        Dead,
+        StaminaRecoveryCooldown
+    }
     #endregion
+    
     #region Unity Method
     void Start()
     {
-        
+        _health = maxHealth;
+        _stamina = maxStamina;
+        _currentSpeed = walkSpeed;
     }
     void Update()
     {
         CameraFollowPlayer();
-        StaminaRegen();
         PlayerMovementHandle();
+        PlayerBarUpdate();
+    }
+
+    private IEnumerator Dash()
+    {
+        _stamina -= dashStaminaDrain;
+        trailEffect.emitting = true;
+        
+        float dashTimeCount = 0;
+        while (dashTimeCount < dashDuration)
+        {
+            SetPlayerStatus(PlayerStatus.Dash);
+            dashTimeCount += Time.deltaTime;
+            _currentSpeed = dashSpeed;
+            yield return null;
+        }
+        
+        trailEffect.emitting = false;
+    }
+    private IEnumerator StaminaRecoveryCooldown()
+    {
+        float timeCount = 0;
+
+        while(timeCount < staminaRecoveryCooldown)
+        {
+            SetPlayerStatus(PlayerStatus.StaminaRecoveryCooldown);
+            timeCount += Time.deltaTime;
+            _currentSpeed = walkSpeed / 2;
+            yield return null;
+        }
+        SetPlayerStatus(PlayerStatus.Clear);
     }
     #endregion
 
     #region method
     private void CameraFollowPlayer()
     {
-        Vector2 cameraPosition = Vector2.SmoothDamp(playerCamera. transform.position, transform.position,
+        Vector3 targetPosition = transform.position + new Vector3(0, 0, -10);
+        double cameraToTargetDistance = Math.Round(Vector3.Distance(targetPosition, playerCamera.transform.position),3);
+        if(cameraToTargetDistance <= 0) return;
+   
+        Vector3 newCameraPosition = Vector3.SmoothDamp(playerCamera.transform.position, targetPosition,
             ref _velocity, cameraSmoothDamp);
 
-        playerCamera.transform.position = new Vector3(cameraPosition.x, cameraPosition.y, -10);
+        playerCamera.transform.position = newCameraPosition;
     }
     private void DashHandle()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl) && !_isDash && stamina >= dashStaminaDrain)
-        {
-            _isDash = true;
-            stamina -= dashStaminaDrain;
-            trailEffect.emitting = true;
-            _dashTime = 0;
-        }
-
-        if (_dashTime < dashDuration)
-        {
-            _dashTime += Time.deltaTime;
-            _currentSpeed = dashSpeed;
-        }
-        else
-        {
-            trailEffect.emitting = false;
-            _isDash = false;
-        }
+        if (!MovementConditionCheck(PlayerStatus.Dash)) return;
+        StartCoroutine(Dash());
     }
-    private void StaminaRegen()
+    private void StaminaRegenHandle()
     {
-        stamina = Mathf.Clamp(stamina, 0, maxStamina);
-        if (!_isDash && !_isSprint && stamina < maxStamina)
-        {
-            stamina += Time.deltaTime * staminaRegen;
-        }
+        _stamina = Mathf.Clamp(_stamina, 0, maxStamina);
+        bool staminaRegenConditionCheck =
+            (!playerStatus.Equals(PlayerStatus.Sprint) && !playerStatus.Equals(PlayerStatus.Dash)) && _stamina < maxStamina;
+            
+        if (!staminaRegenConditionCheck) return;
+        _stamina += Time.deltaTime * staminaRegen;
     }
     private void SprintHandle()
     {
-        if (Input.GetKey(KeyCode.LeftShift) && stamina >= sprintStaminaDrain)
-        {
-            stamina -= Time.deltaTime * sprintStaminaDrain;
-            _currentSpeed = sprintSpeed;
-            _isSprint = true;
-        }
-        else
-        {
-            _isSprint = false;
-        }
+        if (!MovementConditionCheck(PlayerStatus.Sprint)) return;
+        
+        SetPlayerStatus(PlayerStatus.Sprint);
+        _stamina -= Time.deltaTime * sprintStaminaDrain;
+        _currentSpeed = sprintSpeed;
     }
     private void PlayerMovementHandle()
     {
-        _currentSpeed = walkSpeed;
-        SprintHandle();
-        DashHandle();
-        PlayerBarUpdate();
+        StaminaRegenHandle();
+        
         Vector2 playerVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * _currentSpeed;
         playerRigidbody2D.velocity = playerVelocity;
+        
+        if(playerStatus.Equals(PlayerStatus.StaminaRecoveryCooldown)) return;
+        
+        if (playerCamera.velocity == Vector3.zero)
+        {
+            playerStatus = PlayerStatus.Idle;
+            return;
+        }
+
+        if (_stamina <= 0) StartCoroutine(StaminaRecoveryCooldown());
+        
+        SetPlayerStatus(PlayerStatus.Walk);
+        _currentSpeed = walkSpeed;
+        
+        SprintHandle();
+        DashHandle();
     }
     private void PlayerBarUpdate()
     {
-        staminaBar.size = stamina / maxStamina;
-        staminaText.text = $"{stamina:F0} / {maxStamina}";
+        staminaBar.size = _stamina / maxStamina;
+        staminaText.text = $"{_stamina:F0} / {maxStamina}";
+        healthBar.size = _health / maxHealth;
+        healthText.text = $"{_health:F0} / {maxHealth}";
+    }
+    
+    private bool MovementConditionCheck(PlayerStatus status)
+    {
+        switch (status)
+        {
+            case PlayerStatus.Sprint:
+                return Input.GetKey(sprintKey) && _stamina > 0;
+            case PlayerStatus.Dash:
+                return Input.GetKeyDown(dashKey) && !playerStatus.Equals( PlayerStatus.Dash) && _stamina >= dashStaminaDrain;
+            default: return false;
+        }
+    }
+
+    private void SetPlayerStatus(PlayerStatus status)
+    {
+        if(status.Equals(playerStatus)) return;
+        playerStatus = status;
     }
     #endregion
 }
